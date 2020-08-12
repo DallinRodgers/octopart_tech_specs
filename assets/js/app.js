91bdf551fa176fb4b889f55bbe0d9672c0c41eed
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-08-12 17:09:27 -0600

    Create app.js
    javascript for putting content in csv into array

---
+++ assets/js/app.js
@@ -0,0 +1,53 @@
+let fileUploaded = false;
+let allItems = [];
+
+// function myFunction() {
+//   var uploadedFile = document.getElementById("myFile");
+//   var txt = "";
+//   document.getElementById("demo").innerHTML = txt;
+// }
+
+function handleFiles(files) {
+  // Check for the various File API support.
+  if (window.FileReader) {
+    // FileReader are supported.
+    console.log(files);
+    getAsText(files[0]);
+  } else {
+    alert("FileReader are not supported in this browser.");
+  }
+}
+
+function getAsText(fileToRead) {
+  var reader = new FileReader();
+  // Read file into memory as UTF-8
+  reader.readAsText(fileToRead);
+  // Handle errors load
+  reader.onload = loadHandler;
+  reader.onerror = errorHandler;
+}
+
+function loadHandler(event) {
+  var csv = event.target.result;
+  processData(csv);
+}
+
+function processData(csv) {
+  var allTextLines = csv.split(/\r\n|\n/);
+  var lines = [];
+  for (var i = 0; i < allTextLines.length; i++) {
+    var data = allTextLines[i].split(";");
+    var tarr = [];
+    for (var j = 0; j < data.length; j++) {
+      tarr.push(data[j]);
+    }
+    lines.push(tarr);
+  }
+  console.log(lines);
+}
+
+function errorHandler(evt) {
+  if (evt.target.error.name == "NotReadableError") {
+    alert("Canno't read file !");
+  }
+}
